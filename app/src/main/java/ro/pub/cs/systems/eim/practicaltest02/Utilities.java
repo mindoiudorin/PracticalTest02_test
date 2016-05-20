33a0e2a5488e8df300735f4b4fcf1496a0b75e98
Bordin <mindoiu.dorin@fmail.com>
2016-05-20 11:25:10 +0300

    Done

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/Utilities.java
@@ -0,0 +1,22 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+/**
+ * Created by bordin on 5/20/16.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Utilities {
+
+    public static BufferedReader getReader(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public static PrintWriter getWriter(Socket socket) throws IOException {
+        return new PrintWriter(socket.getOutputStream(), true);
+    }
+
+}
